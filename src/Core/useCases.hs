4f7e8ec076c09202ebc4208d3407fba0f99f72d1
Kwaleko <khaledalomar@tutanota.com>
2017-10-26 22:47:07 +0300

    add Update Article service

--- src/Core/useCases.hs
+++ src/Core/useCases.hs
@@ -5,13 +5,11 @@ import Control.Monad.Except
 import Core.Types as T
 
 
-updateArticle ::
-  (T.ArticleRepo m) =>
-     T.UserId
-  -> T.UpdateArticle
-  -> T.Slug
-  -> ExceptT T.UpdateArticleError m T.Article
-updateArticle userId article slug = undefined
+updateArticle :: (T.ArticleRepo m) => T.UserId -> T.UpdateArticle-> T.Slug -> ExceptT T.UpdateArticleError m T.Article
+updateArticle uId article slug = do
+  validateArticleOwnedBy uId slug
+  lift $ T.updateArticleBySlug slug article slug
+  getArticle uId slug
 
 validateArticleOwnedBy :: (T.ArticleRepo m) => T.UserId -> T.Slug  -> ExceptT T.UpdateArticleError m ()
 validateArticleOwnedBy uId slug = do
@@ -20,3 +18,6 @@ validateArticleOwnedBy uId slug = do
     Nothing -> throwError $ T.UpdateArticleErrorNotFound slug
     (Just False) -> throwError $ T.UpdateArticleErrorNotAllowed slug
     _ -> return  ()
+
+getArticle :: T.UserId -> Slug -> ExceptT T.UpdateArticleError m T.Article
+getArticle = undefined
