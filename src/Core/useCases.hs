11a536ecf49f653eda42b6d4caaad2b63427e01d
Kwaleko <khaledalomar@tutanota.com>
2017-10-27 08:43:06 +0300

    update logic

--- src/Core/useCases.hs
+++ src/Core/useCases.hs
@@ -4,20 +4,35 @@ import Control.Monad.Except
 
 import Core.Types as T
 
-
-updateArticle :: (T.ArticleRepo m) => T.UserId -> T.UpdateArticle-> T.Slug -> ExceptT T.UpdateArticleError m T.Article
+-- Articles
+updateArticle :: (T.ArticleRepo m) => T.UserId -> T.UpdateArticle-> T.Slug -> ExceptT T.ArticleError m T.Article
 updateArticle uId article slug = do
   validateArticleOwnedBy uId slug
-  lift $ T.updateArticleBySlug slug article slug
-  getArticle uId slug
+  newSlug <- case T.updateArticleTitle article of
+    Nothing -> return slug
+    Just newTitle -> return $ genSlug slug
+  lift $ T.updateArticleBySlug slug article newSlug
+  getArticle newSlug
 
-validateArticleOwnedBy :: (T.ArticleRepo m) => T.UserId -> T.Slug  -> ExceptT T.UpdateArticleError m ()
+validateArticleOwnedBy :: (T.ArticleRepo m) => T.UserId -> T.Slug  -> ExceptT T.ArticleError m ()
 validateArticleOwnedBy uId slug = do
   result <-lift $ T.isArticleOwnedByUser uId slug
   case result of
-    Nothing -> throwError $ T.UpdateArticleErrorNotFound slug
-    (Just False) -> throwError $ T.UpdateArticleErrorNotAllowed slug
+    Nothing -> throwError $ T.ArticleErrorNotFound slug
+    (Just False) -> throwError $ T.ArticleErrorNotAllowed slug
     _ -> return  ()
 
-getArticle :: T.UserId -> Slug -> ExceptT T.UpdateArticleError m T.Article
-getArticle = undefined
+-- genSlug should be updated to add datetime with user id to the new slug
+-- in order to prevent the slug to be duplicated. Ex: 12-176844600-my-title-post
+genSlug :: T.Title -> T.Slug
+genSlug title = undefined
+
+getArticle :: (T.ArticleRepo m) => Slug -> ExceptT T.ArticleError m T.Article
+getArticle slug = do
+  result <- lift $ T.findArticles (QueryArticleBySlug slug)
+  case result of
+    [] -> throwError $ ArticleErrorNotFound slug
+    lst -> return $ head lst
+
+getArticles :: (ArticleRepo m) =>  ExceptT T.ArticleError m [T.Article]
+getArticles = lift $ T.findArticles All
