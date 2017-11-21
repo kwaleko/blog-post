cbd83c91c751dfa7cbd8dd673c72d5979e2d8cc2
Kwaleko <khaledalomar@tutanota.com>
2017-11-21 20:26:32 +0300

    add server to rest api

--- src/Main.hs
+++ src/Main.hs
@@ -2,9 +2,11 @@
 module Main where
 
 import Elm
+import Servant.Elm
 import Data.Proxy
 
 import qualified Core.Types as T
+import qualified Adapter.API as API
 
 main :: IO ()
 main = undefined
@@ -13,12 +15,32 @@ main = undefined
 spec :: Spec
 spec =
   Spec
-    ["output", "Types"]
-    [ "import Json.Decode exposing (..)"
-    , "import Json.Decode.Pipeline exposing (..)"
-    , toElmTypeSource (Proxy :: Proxy T.Register)
-    , toElmDecoderSource (Proxy :: Proxy T.Register)
-    ]
+    ["", "Types"]
+    (defElmImports
+             : toElmTypeSource    (Proxy :: Proxy T.Register)
+             : toElmDecoderSource (Proxy :: Proxy T.Register)
+             : toElmEncoderSource (Proxy :: Proxy T.Register)
+             : toElmTypeSource    (Proxy :: Proxy T.Article)
+             : toElmDecoderSource (Proxy :: Proxy T.Article)
+             : toElmEncoderSource (Proxy :: Proxy T.Article)
+             : toElmTypeSource    (Proxy :: Proxy T.CreateArticle)
+             : toElmDecoderSource (Proxy :: Proxy T.CreateArticle)
+             : toElmEncoderSource (Proxy :: Proxy T.CreateArticle)
+             : toElmTypeSource    (Proxy :: Proxy T.Auth)
+             : toElmDecoderSource (Proxy :: Proxy T.Auth)
+             : toElmEncoderSource (Proxy :: Proxy T.Auth)
+             : generateElmForAPIWith elmOptions  (Proxy :: Proxy API.API)
+
+    )
+elmOptions :: ElmOptions
+elmOptions =ElmOptions {
+  urlPrefix = Static "http://localhost:8001"
+  ,elmExportOptions = Elm.defaultOptions
+  ,emptyResponseElmTypes = []
+  ,stringElmTypes = []
+            }
+
+
 
 genTypes:: IO ()
-genTypes = specsToDir [spec] "/Users/lambda/development/blog-post/src"
+genTypes = specsToDir [spec] "/Users/lambda/development/blog-post/src/Adapter/FrontEnd/"
