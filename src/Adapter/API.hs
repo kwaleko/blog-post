7190ba3c080c8b0452eb6a92703990e0ddbd34ad
Kwaleko <khaledalomar@tutanota.com>
2017-11-23 17:22:58 +0300

    removed unused function

--- src/Adapter/API.hs
+++ src/Adapter/API.hs
@@ -6,9 +6,14 @@ module Adapter.API where
 --import Data.Either
 import Control.Monad.Reader
 import Control.Monad.Except
-import Data.Aeson(ToJSON(..),FromJSON(..),Value(..),object,(.=))
+import Data.Aeson(ToJSON(..)
+                 ,FromJSON(..)
+                 ,Value(..)
+                 ,object
+                 ,(.=))
 import Data.Proxy(Proxy(..))
 import Data.ByteString.Lazy.Char8(pack)
+import Database.HDBC.Sqlite3(Connection)
 import Network.Wai.Handler.Warp (run)
 import Network.Wai.Middleware.Cors
 import Servant.Client
@@ -19,16 +24,17 @@ import qualified Lib as L
 import qualified Core.Types as T
 import qualified Core.Users as U
 import qualified Core.Articles as A
-import Adapter.SQL  (connect,Connection)
+import qualified Adapter.SQL as S
+
 
 
 type API
-   =   "api" :> "users" :> "register" :> ReqBody '[JSON] T.Register :> Post '[JSON] T.UserId -- Post /api/users/register
-  :<|> "api" :> "users" :> "login" :> ReqBody '[JSON] T.Auth :> Get '[JSON] T.UserId -- Get /api/users/login
-  :<|> "api" :> "articles" :> Capture "userid" T.UserId :> ReqBody '[JSON] T.CreateArticle :> Post '[JSON] T.Article -- (Headers '[Header "Access-Control-Request-Method" String] T.Article)
-  :<|> "api" :> "articles" :> Capture "slug" T.Slug :> Get '[JSON] T.Article -- Get /api/articles/:slug/
-  :<|> "api" :> "articles" :> Get '[JSON]  (Headers '[Header "Access-Control-Allow-Origin" String] [T.Article]) -- Get /api/articles/
---Get '[JSON] (Headers '[Header "X-An-Int" Int] User)
+   =     "api" :> "users"    :> "register"                :> ReqBody '[JSON] T.Register      :> Post '[JSON] T.UserId
+  :<|>   "api" :> "users"    :> "login"                   :> ReqBody '[JSON] T.Auth          :> Post '[JSON] T.UserId
+  :<|>   "api" :> "articles" :> Capture "userid" T.UserId :> ReqBody '[JSON] T.CreateArticle :> Post '[JSON] T.Article
+  :<|>   "api" :> "articles" :> Capture "slug" T.Slug     :> Get '[JSON] T.Article
+  :<|>   "api" :> "articles" :> Get '[JSON]  [T.Article]
+
 
 registerHandler :: Connection -> T.Register ->  Handler T.UserId
 registerHandler conn usr = do
@@ -58,12 +64,12 @@ getArticleHandler conn slug = do
     Left err -> throwError $ liftErr err
     Right article -> return article
 
-getArticlesHandler :: Connection -> Handler ( Headers '[Header "Access-Control-Allow-Origin"  String] [T.Article])
+getArticlesHandler :: Connection -> Handler  [T.Article]
 getArticlesHandler conn = do
   result <- liftIO $ runReaderT (runExceptT A.getArticles) conn
   case result of
     Left err -> throwError $ liftErr err
-    Right articles -> return $ addHeader "*" articles
+    Right articles -> return  articles
 
 appServer :: Connection -> Server API
 appServer conn =
@@ -76,26 +82,15 @@ appServer conn =
 appAPI :: Proxy API
 appAPI = Proxy :: Proxy API
 
---runServer'' :: IO ()
---runServer'' = do
---  conn <- connect
---  run 8001 (runServer conn)-- simpleCors
---  matchAny  "/" $ text "Success"
-runServer''' :: IO ()
-runServer''' = do
-  conn <- connect
+runServer :: IO ()
+runServer = do
+  conn <- S.connect
   run 8001 (app conn)
 
---runServer' :: IO ()
---runServer' = do
---  conn <- connect
---  run 8000 (runServer conn) -- simpleCors
+app conn
+  = cors ( const $ Just (simpleCorsResourcePolicy  { corsRequestHeaders = ["Content-Type"] }) )
+         (serve appAPI (appServer conn))
 
--- simple cors does not allow json contect, instead i will use core
---runServer :: Application
---runServer conn =  simpleCors  (serve appAPI (appServer conn)) -- simpleCors
-
-app conn = cors ( const $ Just (simpleCorsResourcePolicy  { corsRequestHeaders = ["Content-Type"] }) )  (serve appAPI (appServer conn))
 instance FromJSON T.Register
 instance FromJSON T.Auth
 instance FromJSON T.CreateArticle
@@ -105,7 +100,6 @@ instance ToJSON T.Register
 instance ToJSON T.Auth
 instance ToJSON T.CreateArticle
 instance ToJSON T.Article
-
 
 liftErr :: (Show a) =>  a -> ServantErr
 liftErr err = err300 {errBody = pack $ show err}
